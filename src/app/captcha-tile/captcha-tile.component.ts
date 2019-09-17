710a1e3c09284ce818bcb10d6393a80e2a80bc43
shubha-rajan <shubhadayini@google.com>
2019-09-17 16:17:03 -0700

    added colors to show if guesses were correct or incorrect

--- src/app/captcha-tile/captcha-tile.component.ts
+++ src/app/captcha-tile/captcha-tile.component.ts
@@ -10,13 +10,21 @@ export class CaptchaTileComponent implements OnChanges{
   @Input() label: string
   @Input() match: boolean;
   @Input() updateScoreCallback: Function;
-  @Input() gameComplete:boolean;
+  @Input() gameComplete = false;
 
 
   ngOnChanges(){
+      if (this.gameComplete == true){
+        if ((this.selected == true && this.match == true)|| (this.selected == false && this.match == false)) {
+          this.tileClassName = "correct";
+        } else {
+          this.tileClassName = "incorrect";
+        }
+      }
   }
   selected = false;
   className = "not-selected";
+  tileClassName="";
 
 
   toggleSelected() {
